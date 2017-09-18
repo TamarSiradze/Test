b1930ef1b6f8937663238651d97b692f6cac4664
Tamar <tsbusinessnews@gmail.com>
2017-09-18 18:47:00 +0200

    ONE

--- test.R
+++ test.R
@@ -2,3 +2,5 @@ install.packages('ggplot2')
 library(ggplot2)
 ggplot(diamonds, aes(cut)) + geom_bar()
 ggplot(diamonds, aes(cut)) + geom_bar()
+ggplot(diamonds, aes(cut)) + geom_bar
+ggplot(diamonds, aes(cut)) + geom_bar()
