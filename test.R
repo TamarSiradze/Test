0b8568781677e8503f7c5fdbaa34af0dce3225ca
Tamar <tsbusinessnews@gmail.com>
2017-09-19 15:00:08 +0200

    Course 1

--- test.R
+++
@@ -1,6 +0,0 @@
-install.packages('ggplot2')
-library(ggplot2)
-ggplot(diamonds, aes(cut)) + geom_bar()
-ggplot(diamonds, aes(cut)) + geom_bar()
-ggplot(diamonds, aes(cut)) + geom_bar
-ggplot(diamonds, aes(cut)) + geom_bar()
